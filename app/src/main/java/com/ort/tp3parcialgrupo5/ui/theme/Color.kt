54cfe0299ddc9c8354c0d67109ecfcd52bbcffe9
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-10-30 23:23:42 -0300

    Se agregan colores base de la aplicación

--- app/src/main/java/com/ort/tp3parcialgrupo5/ui/theme/Color.kt
+++ app/src/main/java/com/ort/tp3parcialgrupo5/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package com.ort.tp3parcialgrupo5.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Paleta base
+val MainGreen          = Color(0xFF00D09E)
+val LettersAndIcons    = Color(0xFF093030)
+val BgGreenWhite       = Color(0xFFF1FFF3)
+val BgDarkAndLetters   = Color(0xFF052224)
+val BlueButton         = Color(0xFF3299FF)
+val DarkModeGreenBar   = Color(0xFF0E3E3E)
+val DarkModeGreenBlack = Color(0xFF031314)
+val LightBlueButton    = Color(0xFF6DB6FE)
+val LightGreen         = Color(0xFFDFF7E2)
+val OceanBlueButton    = Color(0xFF0068FF)
+val PureBlack          = Color(0xFF000000)
+val PureWhite          = Color(0xFFFFFFFF)
