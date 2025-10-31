b832ef509c25a3c0fd0f33c24ff2552be72470ce
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-10-30 23:25:03 -0300

    MainActivity Actualizado

--- app/src/main/java/com/ort/tp3parcialgrupo5/MainActivity.kt
+++ app/src/main/java/com/ort/tp3parcialgrupo5/MainActivity.kt
@@ -3,27 +3,19 @@ package com.ort.tp3parcialgrupo5
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import com.ort.tp3parcialgrupo5.ui.theme.TP3ParcialGrupo5Theme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TP3ParcialGrupo5Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface {
+                    Greeting("Android")
                 }
             }
         }
@@ -31,11 +23,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(name: String) {
+    val myFont = FontFamily.Default
+    Text(text = "Hello $name!", fontFamily = myFont)
 }
 
 @Preview(showBackground = true)
