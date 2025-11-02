27056746cc4023ed7c1cb9af62debc1511fea7f7
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-11-01 23:37:19 -0300

    Se crean rutas para testing

--- app/src/main/java/com/ort/tp3parcialgrupo5/MainActivity.kt
+++ app/src/main/java/com/ort/tp3parcialgrupo5/MainActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.ort.tp3parcialgrupo5.navigation.AppNavGraph
+import com.ort.tp3parcialgrupo5.navigation.Route
 import com.ort.tp3parcialgrupo5.ui.theme.TP3ParcialGrupo5Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +16,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TP3ParcialGrupo5Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MainScreen() {
+    val navController = rememberNavController()
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TP3ParcialGrupo5Theme {
-        Greeting("Android")
-    }
+    AppNavGraph(
+        navController = navController,
+        startDestination = Route.LaunchInitial.route
+    )
 }
--- app/src/main/java/com/ort/tp3parcialgrupo5/navigation/Route.kt
+++ app/src/main/java/com/ort/tp3parcialgrupo5/navigation/Route.kt
@@ -1,2 +1,13 @@
 package com.ort.tp3parcialgrupo5.navigation
 
+sealed class Route(val route: String) {
+    object LaunchInitial : Route("launch_initial")
+    object LaunchWelcome : Route("launch_welcome")
+    object OnBoarding : Route("onboarding")
+    object Home : Route("home")
+    object Transaction : Route("transaction")
+    object AccountBalance : Route("account_balance")
+    object Notification : Route("notification")
+}
+
+
