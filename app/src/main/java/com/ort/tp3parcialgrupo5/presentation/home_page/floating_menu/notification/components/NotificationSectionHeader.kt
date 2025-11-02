d3efda6e7cfd80d860a6b063a7889725013e0c0d
LeandroCTaddeo <leandrotaddeo03@gmail.com>
2025-11-02 12:36:56 -0300

    encabezado Today/Yesterday/This Weekend

---
+++ app/src/main/java/com/ort/tp3parcialgrupo5/presentation/home_page/floating_menu/notification/components/NotificationSectionHeader.kt
@@ -0,0 +1,13 @@
+package com.ort.tp3parcialgrupo5.presentation.home_page.floating_menu.notification.components
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun NotificationSectionHeader(text: String, modifier: Modifier = Modifier) {
+    Text(text = text, color = Color.White, fontSize = 18.sp, fontWeight = FontWeight.SemiBold, modifier = modifier)
+}
