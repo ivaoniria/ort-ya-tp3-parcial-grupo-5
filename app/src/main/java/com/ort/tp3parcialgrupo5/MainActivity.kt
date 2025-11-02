51c653ad68d3f5c53a04e1bdc8a877b9a2b5011c
gusV15 <gustavov0706@gmail.com>
2025-11-02 15:08:56 -0300

    feat: Se integra el componente Generico BottomNavigationBar.kt

--- app/src/main/java/com/ort/tp3parcialgrupo5/MainActivity.kt
+++ app/src/main/java/com/ort/tp3parcialgrupo5/MainActivity.kt
@@ -11,7 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+
+import com.ort.tp3parcialgrupo5.presentation.components.BottomNavItem
+import com.ort.tp3parcialgrupo5.presentation.components.BottomNavigationBar
+import com.ort.tp3parcialgrupo5.presentation.home_page.home_screen.HomePageScreen
+import com.ort.tp3parcialgrupo5.presentation.home_page.transaction.TransactionScreen
 import com.ort.tp3parcialgrupo5.ui.theme.TP3ParcialGrupo5Theme
+import com.ort.tp3parcialgrupo5.ui.theme.BackgroundContainer
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +29,43 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TP3ParcialGrupo5Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val navController = rememberNavController()
+                val navItems = listOf(
+                    BottomNavItem("Home", R.drawable.home, "home"),
+                    BottomNavItem("Analytics", R.drawable.search, "analytics"),
+                    BottomNavItem("Transactions", R.drawable.transactions, "transactions"),
+                    BottomNavItem("Layers", R.drawable.category, "layers"),
+                    BottomNavItem("Profile", R.drawable.profile, "profile")
+                )
+
+                Scaffold(modifier = Modifier.fillMaxSize(), containerColor = BackgroundContainer, bottomBar = {
+
+                    val currentDestination = navController
+                        .currentBackStackEntryAsState().value?.destination?.route
+
+                    BottomNavigationBar(
+                        items = navItems,
+                        selectedRoute = currentDestination ?: "home",
+                        onItemSelected = { route ->
+                            navController.navigate(route) {
+                                popUpTo(navController.graph.startDestinationId) { saveState = true }
+                                launchSingleTop = true
+                                restoreState = true
+                            }
+                        }
                     )
+                }) { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = "home",
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        composable("home") { HomePageScreen() }
+                        composable("transactions") { TransactionScreen() }
+                        // TODO: Aca se configuran las screens que forman parte de la navegacion del BottomNavBar
+                        //composable("profile") { ProfilePageScreen() }
+                        //composable("category") { CategoryPageScreen() }
+                    }
                 }
             }
         }
