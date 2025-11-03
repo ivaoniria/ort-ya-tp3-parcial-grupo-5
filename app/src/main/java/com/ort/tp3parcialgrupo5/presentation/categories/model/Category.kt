72978487a96038e8848624cea8ed0eea528e9fb3
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-11-02 21:17:02 -0300

    Se crea model para category

---
+++ app/src/main/java/com/ort/tp3parcialgrupo5/presentation/categories/model/Category.kt
@@ -0,0 +1,11 @@
+package com.ort.tp3parcialgrupo5.presentation.categories.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Category(
+    @DrawableRes val iconRes: Int,
+    @StringRes val nameRes: Int,
+    val isSpecialColor: Boolean = false
+)
+
