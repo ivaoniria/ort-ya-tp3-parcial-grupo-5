cb30905cc8dddc67f2af9a6d7f16397454a4fdd2
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-11-04 04:10:23 -0300

    Refactor: Mueve Category a subpaquete model

--- app/src/main/java/com/ort/tp3parcialgrupo5/presentation/categories/model/Category.kt
+++ app/src/main/java/com/ort/tp3parcialgrupo5/presentation/categories/model/Category.kt
@@ -1,4 +1,4 @@
-package com.ort.tp3parcialgrupo5.presentation.categories
+package com.ort.tp3parcialgrupo5.presentation.categories.model
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
