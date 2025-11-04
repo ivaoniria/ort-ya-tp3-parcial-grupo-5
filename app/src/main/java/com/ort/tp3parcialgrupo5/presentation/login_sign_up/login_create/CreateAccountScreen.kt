0810ce4acc38479934a9e4a115819ac15db88dfd
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-11-04 05:30:02 -0300

    feat(login, signup): Create login and create account screens
    - Add `LoginScreen` composable with its components:
      - `LoginTitle` for the welcome message.
      - `LoginTextField` for username and password input.
      - `LoginButton` for login and sign-up actions.
      - `LoginFooter` for alternative login options (fingerprint, social media).
    - Add `CreateAccountScreen` composable for user registration.
      - It includes fields for full name, email, mobile number, date of birth, and password.
      - Adds terms of use and privacy policy text.
      - Provides a link to the login screen for existing users.
    - Implement a reusable `BaseLayout` for consistent screen structure.

---
+++ app/src/main/java/com/ort/tp3parcialgrupo5/presentation/login_sign_up/login_create/CreateAccountScreen.kt
@@ -0,0 +1,171 @@
+package com.ort.tp3parcialgrupo5.presentation.login_sign_up.login_create
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.fillMaxWidth
+import com.ort.tp3parcialgrupo5.R
+import com.ort.tp3parcialgrupo5.presentation.components.BaseLayout
+import com.ort.tp3parcialgrupo5.presentation.login_sign_up.login_create.components.LoginButton
+import com.ort.tp3parcialgrupo5.presentation.login_sign_up.login_create.components.LoginTextField
+import com.ort.tp3parcialgrupo5.ui.theme.AppTypography
+
+@Composable
+fun CreateAccountScreen(
+    onSignUpSuccess: () -> Unit = {},
+    onLoginClick: () -> Unit = {}
+) {
+    var fullName by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var mobileNumber by remember { mutableStateOf("") }
+    var dateOfBirth by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+
+    BaseLayout(
+        contentTop = {
+            Text(
+                text = stringResource(id = R.string.create_account),
+                style = AppTypography.displaySmall,
+                color = Color.White
+            )
+        },
+        contentBottom = {
+            item {
+                LoginTextField(
+                    label = stringResource(id = R.string.full_name),
+                    value = fullName,
+                    onValueChange = { fullName = it },
+                    placeholder = stringResource(id = R.string.placeholder_email),
+                    isPassword = false
+                )
+            }
+            item { Spacer(modifier = Modifier.height(16.dp)) }
+            item {
+                LoginTextField(
+                    label = stringResource(id = R.string.email),
+                    value = email,
+                    onValueChange = { email = it },
+                    placeholder = stringResource(id = R.string.placeholder_email),
+                    isPassword = false
+                )
+            }
+            item { Spacer(modifier = Modifier.height(16.dp)) }
+            item {
+                LoginTextField(
+                    label = stringResource(id = R.string.mobile_number),
+                    value = mobileNumber,
+                    onValueChange = { mobileNumber = it },
+                    placeholder = stringResource(id = R.string.placeholder_mobile_number),
+                    isPassword = false
+                )
+            }
+            item { Spacer(modifier = Modifier.height(16.dp)) }
+            item {
+                LoginTextField(
+                    label = stringResource(id = R.string.date_of_birth),
+                    value = dateOfBirth,
+                    onValueChange = { dateOfBirth = it },
+                    placeholder = stringResource(id = R.string.placeholder_dob),
+                    isPassword = false
+                )
+            }
+            item { Spacer(modifier = Modifier.height(16.dp)) }
+            item {
+                LoginTextField(
+                    label = stringResource(id = R.string.password),
+                    value = password,
+                    onValueChange = { password = it },
+                    placeholder = stringResource(id = R.string.placeholder_password),
+                    isPassword = true
+                )
+            }
+            item { Spacer(modifier = Modifier.height(16.dp)) }
+            item {
+                LoginTextField(
+                    label = stringResource(id = R.string.confirm_password),
+                    value = confirmPassword,
+                    onValueChange = { confirmPassword = it },
+                    placeholder = stringResource(id = R.string.placeholder_password),
+                    isPassword = true
+                )
+            }
+
+            item { Spacer(modifier = Modifier.height(24.dp)) }
+
+            item {
+                val termsAndPolicyString = buildAnnotatedString {
+                    append(stringResource(id = R.string.by_continuing_you_agree) + " ")
+                    withStyle(style = SpanStyle(color = colorResource(id = R.color.main_green))) {
+                        append(stringResource(id = R.string.terms_of_use))
+                    }
+                    append(" and ")
+                    withStyle(style = SpanStyle(color = colorResource(id = R.color.main_green))) {
+                        append(stringResource(id = R.string.privacy_policy))
+                    }
+                }
+                Text(
+                    text = termsAndPolicyString,
+                    textAlign = TextAlign.Center,
+                    color = Color.Gray,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+
+            item { Spacer(modifier = Modifier.height(24.dp)) }
+
+            item {
+                LoginButton(
+                    text = stringResource(id = R.string.sign_up),
+                    onClick = { onSignUpSuccess() },
+                    backgroundColor = colorResource(id = R.color.main_green),
+                    textColor = colorResource(id = R.color.letters_and_icons)
+                )
+            }
+
+            item { Spacer(modifier = Modifier.height(16.dp)) }
+
+            item {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = stringResource(id = R.string.already_have_an_account), color = Color.Gray)
+                    TextButton(onClick = { onLoginClick() }) {
+                        Text(text = stringResource(id = R.string.log_in), color = colorResource(id = R.color.main_green))
+                    }
+                }
+            }
+        }
+    )
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun CreateAccountScreenPreview() {
+    MaterialTheme {
+        CreateAccountScreen()
+    }
+}
+
---
+++ app/src/main/java/com/ort/tp3parcialgrupo5/presentation/login_sign_up/login_create/components/LoginTextField.kt
@@ -0,0 +1,83 @@
+package com.ort.tp3parcialgrupo5.presentation.login_sign_up.login_create.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import com.ort.tp3parcialgrupo5.R
+import com.ort.tp3parcialgrupo5.ui.theme.AppTypography
+
+@Composable
+fun LoginTextField(
+    label: String,
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholder: String,
+    isPassword: Boolean
+) {
+    Text(
+        text = label,
+        style = AppTypography.titleSmall,
+        color = Color.White,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 8.dp, bottom = 6.dp)
+    )
+
+    val bg = colorResource(R.color.bg_green_white)
+    val txt = colorResource(R.color.letters_and_icons)
+
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(35.dp) // controlás la altura total del input
+            .background(bg, RoundedCornerShape(50))
+            .padding(start = 16.dp, end = 12.dp, top = 6.dp, bottom = 6.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            BasicTextField(
+                value = value,
+                onValueChange = onValueChange,
+                singleLine = true,
+                textStyle = AppTypography.bodyMedium.copy(color = txt),
+                visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None,
+                cursorBrush = SolidColor(colorResource(R.color.main_green)),
+                modifier = Modifier.weight(1f)
+            ) { inner ->
+                if (value.isEmpty()) {
+                    Text(
+                        text = placeholder,
+                        style = AppTypography.bodyMedium,
+                        color = txt.copy(alpha = 0.5f)
+                    )
+                }
+                inner()
+            }
+
+            if (isPassword) {
+                Icon(
+                    painter = painterResource(id = R.drawable.eye_pass),
+                    contentDescription = null,
+                    tint = txt
+                )
+            }
+        }
+    }
+}
