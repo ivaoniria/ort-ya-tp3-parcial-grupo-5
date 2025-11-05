e4926c374f3535f6c3aef10182bdbf7a0c2a6cba
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-11-04 23:47:46 -0300

    feat(transactions): Add getTransactionsDirect to TransactionsApi
    This adds the `getTransactionsDirect` suspended function which accepts a full URL to retrieve a list of transactions.

--- app/src/main/java/com/ort/tp3parcialgrupo5/shared/infrastructure/transactions/TransactionsApi.kt
+++ app/src/main/java/com/ort/tp3parcialgrupo5/shared/infrastructure/transactions/TransactionsApi.kt
@@ -3,8 +3,12 @@ package com.ort.tp3parcialgrupo5.shared.infrastructure.transactions
 import com.ort.tp3parcialgrupo5.shared.infrastructure.transactions.dto.TransactionsListResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface TransactionsApi {
     @GET("transactions")
     suspend fun getTransactions(): Response<TransactionsListResponse>
+
+    @GET
+    suspend fun getTransactionsDirect(@Url fullUrl: String): Response<TransactionsListResponse>
 }
