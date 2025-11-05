51dfab54e3aad761508796595cb7a005d107b962
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-11-05 18:13:08 -0300

    refactor(components): Remove unused imports in BaseLayout
    - Remove `MaterialTheme` and `Preview` imports from the `BaseLayout` component as they are no longer in use.

--- app/src/main/java/com/ort/tp3parcialgrupo5/presentation/components/BaseLayout.kt
+++ app/src/main/java/com/ort/tp3parcialgrupo5/presentation/components/BaseLayout.kt
@@ -11,14 +11,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 import com.ort.tp3parcialgrupo5.ui.theme.BackgroundListContainer
