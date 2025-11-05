e2581b8aee199812189f79b63b082b98c9362607
Ivan Liscovsky <liscovsky.ivan@gmail.com>
2025-11-05 04:35:55 -0300

    feat(hilt): Enable Hilt dependency injection
    - Add `@HiltAndroidApp` to a new `TP3App` Application class.
    - Annotate `MainActivity` with `@AndroidEntryPoint` to enable field injection.

--- app/src/main/java/com/ort/tp3parcialgrupo5/MainActivity.kt
+++ app/src/main/java/com/ort/tp3parcialgrupo5/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
 import com.ort.tp3parcialgrupo5.navigation.AppNavGraph
 import com.ort.tp3parcialgrupo5.ui.theme.TP3ParcialGrupo5Theme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/ort/tp3parcialgrupo5/TP3App.kt
@@ -0,0 +1,8 @@
+package com.ort.tp3parcialgrupo5
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TP3App : Application()
+
